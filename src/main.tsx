3ed1a86f39b624c7f5a812c3102b89621315cd42
UzielMedina22 <uziel.jonathan22@gmail.com>
2026-02-11 02:24:06 -0700

    Fix route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createHashRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router'
 import './index.css'
 import Inicio from './pages/Inicio.tsx'
 import Juego from './pages/Juego.tsx'
 
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     element: <Inicio />
